d1ca92f9fd93f06d9220262ceeb626a7c866423b
Guilherme Marcos <58338699+GuiMarcos@users.noreply.github.com>
2023-06-21 20:18:39 -0300

    0.3 - About Section Made and some bug fix in Nav

--- js/basic.js
+++ js/basic.js
@@ -1,13 +1,31 @@
 function showNav() {
 	if (navbarCollapsed.style.display == "flex") {
 		navbarCollapsed.style.display = "none";
-		navMain.style.display = "flex";
-		animationParticle.style.display = "block";
-		arrow.style.display = "block";
+		visibleContent.style.display = "block";
+		document.querySelector(".is-active").scrollIntoView({
+			behavior: "smooth",
+		});
 	} else {
+		isElementVisible(document.getElementById("home"));
+		isElementVisible(document.getElementById("about"));
+		//isElementVisible(skills);
+		//isElementVisible(portfolio);
+		//isElementVisible(contact);
 		navbarCollapsed.style.display = "flex";
-		navMain.style.display = "none";
-		animationParticle.style.display = "none";
-		arrow.style.display = "none";
+		visibleContent.style.display = "none";
 	}
+}
+
+function isElementVisible(element) {
+	const rect = element.getBoundingClientRect();
+	if (
+		rect.top >= 0 &&
+		rect.left >= 0 &&
+		rect.bottom <=
+			(window.innerHeight || document.documentElement.clientHeight) &&
+		rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+	)
+		element.classList.add("is-active");
+	else element.classList.remove("is-active");
+	return;
 }
