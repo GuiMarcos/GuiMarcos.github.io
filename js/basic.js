bece78eb163d052ac31cf6b13748054650338004
GuiMarcos <guilherme.amarcos@gmail.com>
2023-06-20 23:12:41 -0300

    0.2 - Fix arrow and Nav

--- js/basic.js
+++ js/basic.js
@@ -3,9 +3,11 @@ function showNav() {
 		navbarCollapsed.style.display = "none";
 		navMain.style.display = "flex";
 		animationParticle.style.display = "block";
+		arrow.style.display = "block";
 	} else {
 		navbarCollapsed.style.display = "flex";
 		navMain.style.display = "none";
 		animationParticle.style.display = "none";
+		arrow.style.display = "none";
 	}
 }
