2c2e0aa2f7bb4f0fcd62ba3972382ae3d8060323
GuiMarcos <guilherme.amarcos@gmail.com>
2023-06-20 23:06:05 -0300

    0.1 - Home Page and Navigation Bar made
    Signed-off-by: GuiMarcos <guilherme.amarcos@gmail.com>

---
+++ js/basic.js
@@ -0,0 +1,11 @@
+function showNav() {
+	if (navbarCollapsed.style.display == "flex") {
+		navbarCollapsed.style.display = "none";
+		navMain.style.display = "flex";
+		animationParticle.style.display = "block";
+	} else {
+		navbarCollapsed.style.display = "flex";
+		navMain.style.display = "none";
+		animationParticle.style.display = "none";
+	}
+}
